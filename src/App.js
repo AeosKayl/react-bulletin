fbae0bcbbcc5488ebb03e6569cb1969058f89c61
AeosKayl <prosium@gmail.com>
2022-09-16 15:16:51 +0200

    updated with components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import BulletinBoard from './components/BulletinBoard';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h1>
+          React Bulletin Board
+        </h1>
+
       </header>
+      <main className='App-header'>
+        <BulletinBoard/>
+      </main>
+      <footer>
+        <p>A footer should be here!</p>
+      </footer>
+
     </div>
   );
 }
